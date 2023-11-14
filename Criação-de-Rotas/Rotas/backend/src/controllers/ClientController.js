57d746d93fed04d4c28c9bc40ec9484a6f269c28
Frankdias92 <frankmcdias@gmail.com>
2023-11-14 17:56:17 -0300

    atualizando arquivos

--- Criação-de-Rotas/Rotas/backend/src/controllers/ClientController.js
+++ Criação-de-Rotas/Rotas/backend/src/controllers/ClientController.js
@@ -19,4 +19,43 @@ getOneClient = (req, res) => {
   res.status(200).send(client);
 }
 
-module.exports = {getClients, getOneClient};
+createClient = (req, res) => {
+  const client = req.body;
+
+  if (Object.keys(client).length > 0) {
+    clientes.push(client);
+    res.status(201).send(client);
+  } else {
+    res.status(406).send("Não foi possivel adicionar esse cliente!")
+  }
+
+}
+
+updateClient = (req, res) => {
+  let id = req.params.id;
+  let indice = findClientIndex(id);
+
+  if (indice !== -1) {
+    clientes[indice] = req.body;
+    res.status(201).send("Cliente Atualizado!");
+  } else {
+    res.status(406).send("Não foi possivel atualizar o cliente! Verifique e tente novamente.");
+  }
+
+}
+
+removeClient = (req, res) => {
+  let id = req.params.id;
+  let indice = findClientIndex(id);
+  clientes.splice(indice, 1);
+  res.status(200).send(`O Cliente de ID ${id} foi removido!`);
+}
+
+
+
+function findClientIndex(id) {
+  const indice = clientes.findIndex((item) => item.id === Number(id));
+  return indice;
+}
+
+module.exports = {getClients, getOneClient, createClient, updateClient, removeClient};
--- Criação-de-Rotas/Rotas/backend/src/routes/clientRoute.js
+++ Criação-de-Rotas/Rotas/backend/src/routes/clientRoute.js
@@ -4,8 +4,10 @@ const clientController = require('./../controllers/ClientController');
 
 
 router.get('/', clientController.getClients);
-
 router.get('/:id', clientController.getOneClient);
+router.post('/', clientController.createClient);
+router.put('/:id', clientController.updateClient);
+router.delete('/:id', clientController.removeClient);
 
 
 module.exports = router;
